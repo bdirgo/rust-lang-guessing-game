bb30076205a0f37f84f74204cce3c6682c737458
Benjamin Dirgo <benjamindirgo@Benjamins-MacBook-Air.local>
2018-02-09 13:52:44 -0700

    Add frist step in guessing game, ask for a number that the user guesses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess The number!");
+    println!("Please input now:");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line!");
+
+    println!("You guessed: {}", guess);
 }
