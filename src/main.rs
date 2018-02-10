d55f6a18594ec52f6d6973239845d371162677e4
Benjamin Dirgo <benjamin.dirgo@gm.com>
2018-02-09 22:47:38 -0700

    Update main to use rand Crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess The number!");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("Secret number is: {}", secret_number);
     println!("Please input now:");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line!");
-
     println!("You guessed: {}", guess);
 }
