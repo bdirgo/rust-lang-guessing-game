b5299d0e98bfd9f3c99f5e91a3a2f81cdd87ce28
Benjamin Dirgo <benjamin.dirgo@gm.com>
2018-02-10 03:15:54 -0700

    Draw the owl

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess The number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("Secret number is: {}", secret_number);
-    println!("Please input now:");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line!");
-    println!("You guessed: {}", guess);
+
+    loop {
+	    println!("Please input now:");
+	    let mut guess = String::new();
+	    io::stdin().read_line(&mut guess)
+	        .expect("Failed to read line!");
+
+	    let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue
+	    };
+
+	    println!("You guessed: {}", guess);
+
+	    match guess.cmp(&secret_number) {
+	    	Ordering::Less => println!("Too small!"),
+	    	Ordering::Greater => println!("Too big!"),
+	    	Ordering::Equal => {
+	    		println!("You Win!!!");
+	    		break;
+	    	}
+	    }
+	}
 }
